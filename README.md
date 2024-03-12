09da82ddc5ad812f802737f21782b32b91bbc8fb
Jess Ubuntu <jjk14150@gmail.com>
2024-03-11 21:26:38 -0700

    Completed Semaphore program

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Semaphore
+# Semaphore and Mutex
+
+## Semaphore program
+
+This simple program shows how to restrict threads from entering into a critical area of code by using semaphores to limit the amount of threads that can in the critical area at one time. I restrict them to 10 by with this code:
+`semaphore = sem_open("Semaphore", O_CREAT, 00644, 10);`
+10 as the last argument means that this semaphore will allow up to 10 concurrent threads into the critical section.
+Then, when the threads are created using a for loop and pthread_create, they all begin running the connect() function. The connect() function contains the critical region. sem_wait allows a thread in and then decrements the number of available openings that are left. When there are 10 threads in the critical region, then it stops letting threads in. When a thread is finished in the critical region, they call the sem_post function which adds 1 to the available opening and then the semaphore lets a new thread in.
+
+Video:
+
+[![Semaphore video](../Screenshot/semaphore_video.png)](https://www.loom.com/share/529a9e4132354dc1bef3643f2e732a0b?sid=9f6ae12a-7944-4938-adeb-1221b240c993)
--- semaphore.c
+++ semaphore.c
@@ -16,8 +16,9 @@ sem_t* semaphore;
 // Thread function to simulate connection to database
 void *connect(void *arg)
 {
-    int threadNumber = *((int *) arg);
+    int threadNumber = *((int *) arg); // get the int threadNumber from args
     int x, tmp;
+    int random_time = rand() % 10 + 1; // Generate a random number between 1 and 10
 
     // *** Start of Critical Region ***
     // lock it down for 10 threads at a time
@@ -26,17 +27,21 @@ void *connect(void *arg)
     currentAmountOfUsers = currentAmountOfUsers + 1;
     printf("Current users: %d\n", currentAmountOfUsers);
 
-    int random_time = rand() % 10 + 1; // Generate a random number between 1 and 10
-    printf("Thread #%d entered connection pool and waiting for %d seconds...\n", threadNumber, random_time);
-    
+    printf("Thread #%d entered connection pool and waiting for %d seconds...\n", 
+        threadNumber, random_time);
+    sleep(random_time); // Sleep for the random time
 
+    printf("Thread #%d exiting connection pool\n", threadNumber);
+
+    // decrement the current users within the critical section
+    currentAmountOfUsers = currentAmountOfUsers - 1;
+
+    printf("Current users: %d\n", currentAmountOfUsers);
+ 
     // *** End of Critical region ***
     // Unlock
     sem_post(semaphore);
 
-    printf("Thread #%d exiting connection pool\n", threadNumber);
-    currentAmountOfUsers = currentAmountOfUsers - 1;
-    printf("Current users: %d\n", currentAmountOfUsers);
     return NULL;
 }
 
@@ -60,7 +65,7 @@ int main()
     for(int i = 0; i < TOTAL_USERS; ++i)
     {
         thread_args[i] = i;
-        if(pthread_create(&tid[i], NULL, connect, &i))
+        if(pthread_create(&tid[i], NULL, connect, &thread_args[i]))
         {
             // if error
             printf("\n ERROR creating deposit thread %d\n", i);
